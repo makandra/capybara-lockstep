8bb902cca2b7c55deccc942897fb32755298eceb
Henning Koch <henning.koch@makandra.de>
2025-03-17 14:36:42 +0100

    Clean up spec locations

--- spec/lib/capybara-lockstep/lockstep_spec.rb
+++ spec/lib/capybara-lockstep/lockstep_spec.rb
@@ -30,5 +30,20 @@ module Capybara
 
     end
 
+    describe ".debug" do
+      before { Capybara::Lockstep.debug = nil }
+      after { Capybara::Lockstep.debug = nil }
+
+      it "accepts true" do
+        expect {
+          Capybara::Lockstep.debug = true
+        }.to output(/STDOUT/).to_stdout
+      end
+
+      it "accepts a logger" do
+        Capybara::Lockstep.debug = Logger.new("/dev/null")
+      end
+    end
+
   end
 end
